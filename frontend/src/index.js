1fc33f3981227a63367c1beef263adbfa121ec93
Owaisraza <owaisharoon00@gmail.com>
2025-06-18 12:24:51 +0500

    commited

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 
 // Use environment variable (works both locally and on Render)
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+console.log(process.env.REACT_APP_API_URL)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
